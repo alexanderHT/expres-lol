7d8f330ed9d858ce2102a0a8bbd1951f5170c03b
alexanderHT <alexanderhendrawan@gmail.com>
2016-10-28 21:18:07 +0700

    express setup

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 'use strict'
 
+// node modules
+
+// express dependencies
 const express = require('express');
+const body-parser = require('body-parser');
+const cors = require('cors');
+
+// initial express
+const app = express()
+const router = app.Router()
+
+// app configuration
